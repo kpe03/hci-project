dccd02772a2fecf1a1d4dba74c19fcc093d95364
KT <kaitlynrpeters@gmail.com>
2025-02-25 13:39:38 -0600

    started navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <p>Home page</p>
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Link from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <h1 className="navbar-logo">ACM</h1>
+            {/* <ul className="navbar-links">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/services">Services</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+            </ul> */}
+        </nav>
+    );
+}
+
+export default Navbar;
