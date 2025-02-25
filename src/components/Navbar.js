967d335b787220cc65628660df15308aac3fe46f
KT <kaitlynrpeters@gmail.com>
2025-02-25 14:49:36 -0600

    navbar update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import Link from 'react-router-dom';
+import './Navbar.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Navbar() {
     return (
-        <nav className="navbar">
-            <h1 className="navbar-logo">ACM</h1>
-            {/* <ul className="navbar-links">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/services">Services</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-            </ul> */}
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="navbar-nav d-flex flex-row align-items-center">
+                <div class="d-flex justify-content-start w-50">
+                    <a class="nav-link" href="/" role="button">Association for Computing Machinery</a>
+                </div>
+                <div className="navbar-nav d-flex flex-row m-3">
+                    <a className="nav-link m-1" href="/about" role="button">About</a>
+                    <a className="nav-link m-1" href="/contact" role="button">Contact</a>
+                </div>
+            </div>
         </nav>
     );
 }
