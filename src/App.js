68e1ac490417d6829506621b4e9c2159b72ceca3
KT <kaitlynrpeters@gmail.com>
2025-02-25 23:03:14 -0600

    added a carousel with default stock images

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
+import Carousel from './components/Carousel';
 
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Carousel />
       <p>Home page</p>
     </div>
   );
