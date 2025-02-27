070a867fab75c212f79dc87bfc9cb39b46d88dc2
cth2x <colehicks50@gmail.com>
2025-02-26 23:20:18 -0600

    Login page completed, cleaned up unnecessary code

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Login.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [rememberMe, setRememberMe] = useState(false);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Logic for submission form
-    console.log("Login attempted with:", { username, password, rememberMe });
-  };
-
   return (
     <div className="login-page">
       <div className="breadcrumb">
@@ -26,40 +16,27 @@ const Login = () => {
           <h2>
             <FontAwesomeIcon icon={faUser} /> Sign into your ACM Account
           </h2>
-          <form onSubmit={handleSubmit}>
+          <form>
             <div className="input-group">
               <label>Username</label>
-              <input
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                placeholder="Username"
-              />
+              <input type="text" placeholder="Username" />
               <a href="/forgot-username" className="forgot-link">
-                Forgot username
+                Forgot username?
               </a>
             </div>
 
             <div className="input-group">
               <label>Password</label>
-              <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Password"
-              />
-              <a href="/forgot-password" className="forgot-link">
-                Forgot password
-              </a>
-            </div>
-
-            <div className="remember-me">
-              <input
-                type="checkbox"
-                checked={rememberMe}
-                onChange={(e) => setRememberMe(e.target.checked)}
-              />
-              <label>Remember me</label>
+              <input type="password" placeholder="Password" />
+              <div className="password-options">
+                <div className="remember-me">
+                  <input type="checkbox" />
+                  <label>Remember me</label>
+                </div>
+                <a href="/forgot-password" className="forgot-link">
+                  Forgot password?
+                </a>
+              </div>
             </div>
 
             <button type="submit" className="sign-in-button">
