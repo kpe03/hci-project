3a7802223b79aaaa92b64ce665643a757309ccf7
Thang Nguyen <thang.q.nguyen-1@ou.edu>
2025-02-28 00:30:29 -0600

    add section items into footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,66 +4,215 @@ import './Footer.css'
 import logo from '../acm.png';
 
 function Footer() {
-    return (
-        <footer className="footer bg-dark text-white">
-          <div className="container-fluid p-0">
-            {/* Top Row - Association for Computing Machinery */}
-            <div className="row g-0">
-              <div className="col-12">
+  return (
+    <footer className="footer bg-dark text-white">
+      <div className="top container-fluid p-0">
+        {/* Top Row - Association for Computing Machinery */}
+        <div className="row g-0">
+          <div className="col-12">
+            <a
+              className="nav-link py-3 footer-title"
+              href="/"
+              role="link"
+            >
+              Association for Computing Machinery
+            </a>
+          </div>
+        </div>
+        {/* END Top Row */}
+
+        {/* Bottom Row - Navigation Links */}
+        <div className="row g-0">
+          <div className="col">
+            {/* Main navigation row */}
+            <div className="d-flex flex-row align-items-start">
+              {/* About section with its submenu */}
+              <div className="nav-item about-container me-3">
                 <a
-                  className="nav-link py-3 navbar-title"
-                  href="/"
-                  role="link"
-                >
-                  Association for Computing Machinery
-                </a>
-              </div>
-            </div>
-            
-            {/* Bottom Row - Navigation Links */}
-            <div className="row g-0">
-              <div className="col d-flex flex-row align-items-center">
-                <a
-                  className="nav-link py-2 me-3"
+                  className="nav-link py-2 main-nav-link"
                   href="/about"
                   role="link"
-                  style={{ paddingLeft: '15px' }}
                 >
                   About
                 </a>
+
+                {/* About submenu (structurally connected to its parent) */}
+                <div className="about-submenu">
+                  <a
+                    className="nav-link py-1 about-submenu-item"
+                    href="/"
+                    role="link"
+                  >
+                    About ACM
+                  </a>
+                  <a
+                    className="nav-link py-1 about-submenu-item"
+                    href="/"
+                    role="link"
+                  >
+                    Membership
+                  </a>
+                  <a
+                    className="nav-link py-1 about-submenu-item"
+                    href="/"
+                    role="link"
+                  >
+                    MyACM Sign In
+                  </a>
+                  <a
+                    className="nav-link py-1 about-submenu-item"
+                    href="/"
+                    role="link"
+                  >
+                    Volunteer
+                  </a>
+                </div>
+              </div>
+
+              {/* Publications section */}
+              <div className="footer-item publications-container me-3">
                 <a
-                  className="nav-link py-2 me-3"
+                  className="nav-link py-2 main-nav-link"
                   href="/publications"
                   role="link"
                 >
                   Publications
                 </a>
+                <div className="publications-submenu">
+                  <a
+                    className="nav-link py-1 publications-submenu-item"
+                    href="/"
+                    role="link"
+                  >
+                    About Publications
+                  </a>
+
+                  <a
+                    className="nav-link py-1 publications-submenu-item"
+                    href="/"
+                    role="link"
+                  >
+                    Digital Library
+                  </a>
+
+                  <a
+                    className="nav-link py-1 publications-submenu-item"
+                    href="/"
+                    role="link"
+                  >
+                    Submit a Paper
+                  </a>
+                </div>
+              </div>
+
+              {/* Community section */}
+              <div className="nav-item community-container main-nav-link">
                 <a
-                  className="nav-link py-2 me-3"
+                  className="nav-link py-2 me-3 align-self-start"
                   href="/community"
                   role="link"
                 >
                   Community
                 </a>
+
+                <div className="community-submenu">
+                  <a
+                    className="nav-link py-1 community-submenu-item"
+                    href="/"
+                    role="link"
+                  >
+                    Special Interest Groups
+                  </a>
+                  <a
+                    className="nav-link py-1 community-submenu-item"
+                    href="/"
+                    role="link"
+                  >
+                    Chapters
+                  </a>
+
+                </div>
+              </div>
+              {/* Education section */}
+              <div className="nav-item education-container main-nav-link">
                 <a
-                  className="nav-link py-2 me-3"
+                  className="nav-link py-2 me-3 align-self-start"
                   href="/education"
                   role="link"
                 >
                   Education
                 </a>
-                <a
-                  className="nav-link py-2"
-                  href="/contact"
-                  role="link"
-                >
-                  Connect with Us
-                </a>
+
+                <div className="education-submenu">
+                  <a
+                    className="nav-link py-1 education-submenu-item"
+                    href="/"
+                    role="link"
+                  >
+                    Education Center
+                  </a>
+                </div>
               </div>
+              <a
+                className="nav-link py-2 align-self-start main-nav-link"
+                href="/contact"
+                role="link"
+              >
+                Connect with Us
+              </a>
+              
+            {/* INSERT THE IMAGES HERE */}
+
             </div>
           </div>
-        </footer>
-      );
+        </div>
+        {/* END Bottom Row*/}
+
+      </div>
+      {/* End Options */}
+      <div className="bottom container-fluid p-0">
+        <div className="row g-0">
+          <div className="bottom-row d-flex flex-row align-items-start">
+            <a
+              className="underline nav-link py-3 contactus me-3"
+              href="/"
+              role="link"
+            >
+              Contact Us
+            </a>
+            <a
+              className="underline nav-link py-3 privacypolicy me-3"
+              href="/"
+              role="link"
+            >
+              Privacy Policy
+            </a>
+
+            <a
+              className="underline nav-link py-3 socialmediapolicy me-3"
+              href="/"
+              role="link"
+            >
+              Social Media Policy
+            </a>
+
+            <a
+              className="nav-link py-3 copyright me-3"
+              href="/"
+              role="link"
+            >
+              Copyright [INSERT SYMBOL HERE] 2025, ACM, Inc
+            </a>
+
+          </div>
+          <div className="col-12">
+
+          </div>
+        </div>
+      </div>
+    </footer >
+
+  );
 }
 
 export default Footer;
