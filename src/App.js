fffc5aa46a7cbcad61d5b60f417962e8968f79bf
Mounir.Zah <Mounir.Zahidi-1@ou.edu>
2025-02-28 15:44:43 -0600

    Added a new Profile Navbar, Added link from the Logout button to Login Page, fixed the position of the buttons on the profile page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
+import ProfileNavbar from "./components/ProfileNavbar";
 import Login from "./components/Login";
 import Footer from "./components/Footer";
 import Carousel from "./components/Carousel";
 import Profile from "./components/Profile";
 
+function AppContent() {
+  const location = useLocation(); // Detect current route
+
+  return (
+    <div className="App">
+      {/* Conditionally Render Navbar */}
+      {location.pathname === "/profile" ? <ProfileNavbar /> : <Navbar />}
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Carousel />
+              <p>Home page</p>
+            </>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+
+      <Footer />
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Carousel />
-                <p>Home page</p>
-              </>
-            }
-          />
-          <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<Profile />} /> {/* Added Profile Route */}
-        </Routes>
-        <Footer />
-      </div>
+      <AppContent />
     </Router>
   );
 }
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -3,7 +3,7 @@ import React from "react";
 const UserInfo = () => {
   return (
     <div className="user-info">
-      <p>SERVICES - Full Name LOOOOOL</p> {/* Added Full Name Placeholder */}
+      <p>SERVICES - Full Name</p> {/* Added Full Name Placeholder */}
       <h2>Client Number: <strong>1234567</strong></h2>
     </div>
   );
