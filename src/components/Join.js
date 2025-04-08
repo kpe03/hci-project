aa134a8acd0ae988aeb8d98f87638d79c7e16dac
cth2x <colehicks50@gmail.com>
2025-04-08 12:10:24 -0500

    Fixed issue in Join.js

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import acmLogo from '../assets/images/acm-logo.png';
 import './Join.css';
 
 const Join = () => {
@@ -10,6 +11,17 @@ const Join = () => {
           Home
         </Link>{' '}
         {`> Join`}
+        <span className="breadcrumb-note"></span>
+      </div>
+
+      <div className="join-container">
+        <div className="join-box">
+          <div className="acm-logo-container">
+            <img src={acmLogo} alt="ACM Logo" className="acm-logo" />
+            <h2 className="join-acm-text">Join ACM</h2>
+          </div>
+          <div className="input-group" />
+        </div>
       </div>
     </div>
   );
