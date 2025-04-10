9eb917aacc52c498f36968d30f5d201f121e4f6f
KaitlynP <kaitlynrpeters@gmail.com>
2025-04-10 14:39:56 -0500

    added HomePage.js for the carousel and other home stuff

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,18 +1,22 @@
 import React, { useContext } from "react";
 import { AuthContext } from "./AuthContext"; 
 import { useState, useEffect } from "react";
-
+import Carousel from "./Carousel";
 
 const HomePage = () => {
     const { isLoggedIn } = useContext(AuthContext);
 
   return (
     <div>
-      {isLoggedIn ? (
+      {/* {isLoggedIn ? (
         <p>Welcome, logged in user!</p>
       ) : (
         <p>You are not logged in.</p>
-      )}
+      )} */}
+      <>
+        <Carousel />
+        <p>Home page</p>
+        </>
     </div>
   );
 }
