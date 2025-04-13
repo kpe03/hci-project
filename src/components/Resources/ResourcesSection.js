052b9f00715bb21a8cbbc865c00fcddbf333087e
Emma Smith <emma.k.smith-1@ou.edu>
2025-04-12 21:32:15 -0500

    update resources section styling

--- src/components/Resources/ResourcesSection.js
+++ src/components/Resources/ResourcesSection.js
@@ -34,6 +34,7 @@ const Resources = () => {
     return (
       <div className="resource-section">
         <h2 className="resource-heading">Resources</h2>
+        <hr style={{ color: "#2D9CDB", width: "75%", margin: "0 auto", marginBottom: "3rem"}} />
         <div className="resource-grid">
           {resourceData.map((res, idx) => (
             <ResourceCard key={idx} {...res} />
