55f821876c87c5cee27b459f3389e6c64a10c04d
Mounir Zahidi <Mounir.Zahidi-1@ou.edu>
2025-04-14 15:46:59 -0500

    User stays logged in when page is refreshed

--- src/components/AuthContext.js
+++ src/components/AuthContext.js
@@ -1,9 +1,16 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(() => {
+    const storedStatus = localStorage.getItem('isLoggedIn');
+    return storedStatus ? JSON.parse(storedStatus) : false;
+  });
+  
+  useEffect(() => {
+    localStorage.setItem('isLoggedIn', JSON.stringify(isLoggedIn));
+  }, [isLoggedIn]);
 
   return (
     <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
